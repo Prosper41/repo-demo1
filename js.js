9d5f8398ed3d9e1dd9745bfdd9cb91a06fe2d262
Prosper <106637040+Prosper41@users.noreply.github.com>
2024-04-18 01:59:21 +0000

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+// Numbers
+const myNumber = 43
+const myFloat = 43.01
+const myString = "43.122ass";
+
+
+// console.log(Number(myString) === myNumber);
+
+// Number Methods
+// console.log(Number.parseFloat(myString));
+// console.log(Number.isInteger(myString));
+// console.log(Number.parseFloat(myString).toFixed(2));
+// console.log(typeof Number.parseFloat("2.1223hd").toFixed(2).toString());
+
+
+console.log(isNaN("Baba"));
